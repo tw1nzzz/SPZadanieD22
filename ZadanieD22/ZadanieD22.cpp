211eaf88d9f9b6fbff768e0316ff20af3e81cb39
Ксения Твинз <ksenia.twinz@gmail.com>
2020-11-27 23:39:09 +0300

    Добавьте файлы проекта.

---
+++ ZadanieD22/ZadanieD22.cpp
@@ -0,0 +1,113 @@
+﻿#define _USE_MATH_DEFINE
+#include<Windows.h>
+#include<tchar.h>
+#include <cmath>
+const int WIDTH =50;
+const int HEIGHT =1000;
+int a = 2, b = -3;
+int function(double x) {
+	return a * pow(x, 4) + b * pow(x, 3);
+}
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+	HWND hMainWnd;
+	TCHAR szClassName[] = _T("MyClass");
+	MSG msg;
+	WNDCLASSEX wc;
+	//Заполняем структуру класса окна
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = szClassName;
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	//Регистрируем класс окна
+	if (!RegisterClassExW(&wc)) {
+		MessageBoxW(NULL, _T("Cannot registr class"), _T("Error"), MB_OK);
+		return 0;
+	}
+	//Создаем основное окно приложения
+	hMainWnd = CreateWindow(szClassName, _T("ZadanieD22"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL,
+		hInstance, NULL);
+
+	if (!hMainWnd) {
+		MessageBox(NULL, _T("Cannot create main window"), _T("Error"), MB_OK);
+		return 0;
+	}
+	//Показываем окно
+	ShowWindow(hMainWnd, nCmdShow);
+	//UpdateWindow(hMainWnd);
+	//Выполняем цикл обработки сообщений до закрытия окна
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessageW(&msg);
+	}
+	return msg.wParam;
+	char path[100];
+
+
+
+}
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam,
+
+	LPARAM lParam) {
+	HDC hDC;
+	PAINTSTRUCT ps;
+	RECT rect;
+	static HPEN hpen1, hpen2;
+	static int sx, sy;
+	double x;
+	int y;
+
+
+	switch (uMsg) {
+	case WM_CREATE:
+		hpen1 = CreatePen(PS_SOLID, 0.5, RGB(0, 0, 0));
+		hpen2 = CreatePen(PS_SOLID, 0.5, RGB(255, 105, 180));
+		break;
+	case WM_SIZE:
+		sx = LOWORD(lParam);
+		sy = HIWORD(lParam);
+		//MoveWindow(hWnd, 0, 0, 500, 500, TRUE);
+		break;
+	case WM_PAINT:
+		hDC = BeginPaint(hWnd, &ps);
+		SetMapMode(hDC, MM_ANISOTROPIC);
+		SetWindowExtEx(hDC, 2 * WIDTH, 2 * -HEIGHT, NULL);
+		SetViewportExtEx(hDC, sx / 2, sy / 2, NULL);
+		SetViewportOrgEx(hDC, sx / 2, sy / 2, NULL);
+		SelectObject(hDC, hpen1);
+		MoveToEx(hDC, -WIDTH*2, 0, NULL);
+		LineTo(hDC, WIDTH*2, 0);
+		MoveToEx(hDC, 0, 0, NULL);
+		MoveToEx(hDC, 0, -HEIGHT*2, NULL);
+		LineTo(hDC, 0, HEIGHT*2);
+		SelectObject(hDC, hpen2);
+		MoveToEx(hDC, -200, 0, NULL);
+		for (x = -200; x <= 200; x += 1.0f / 100)
+		{
+			y = function(x);
+			if(y < HEIGHT/2)
+				LineTo(hDC, x, y * 100);
+		}
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		DeleteObject(hpen2);
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, uMsg, wParam, lParam);
+	}
+	return 0;
+
+}
